6274f819f8869a22ecac315e6185e6f9d4c5a7d3
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-11 21:44:14 -0600

    added broweser router, NashvilleHotIndex.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
+import { NashvilleHotIndex } from './components/NashvilleHotIndex.js';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <NashvilleHotIndex />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
