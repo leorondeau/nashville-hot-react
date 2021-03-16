278e381b04383fa5182cef994eec48a228977678
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-16 14:22:56 -0500

    added restaurantlist path and list function. Detail mod also added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,25 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { RestaurantProvider } from "./restaurant/RestaurantProvider.js"
+import {RestaurantList} from './restaurant/RestaurantList'
+import { RestaurantDetail } from './restaurant/RestaurantDetail'
 
-export const ApplicationViews = () => {
+export const ApplicationViews = (props) => {
     return <>
         <main style={{
             margin: "5rem 2rem",
             backgroundColor: "lightgoldenrodyellow"
         }}>
-            Application views
+            <RestaurantProvider>
+                <Route path="/" render={(props) => {
+                   return <RestaurantList {...props}/> 
+                }}>
+                </Route>
+                <Route path="/restaurant/" render={(props) => {
+                   return <RestaurantDetail {...props}/> 
+                }}>
+                </Route>
+            </RestaurantProvider>
         </main>
     </>
 }
