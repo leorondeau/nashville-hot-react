5b283fea6f0b2cc9a0e65cf3f4e88b3e54a3cfa9
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-17 21:55:35 -0500

    added image and website to rended upon restaurant selection

--- src/components/restaurant/RestaurantDetail.js
+++ src/components/restaurant/RestaurantDetail.js
@@ -1,23 +1,26 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import { RestaurantContext } from './RestaurantProvider'
 import { Link } from "react-router-dom"
+import "./Restaurant.css"
+import "../profile/Profile.css"
 
 export const RestaurantDetail = (props) => {
     const { restaurant, getRestaurantById } = useContext(RestaurantContext)
     // const [restaurant, setRestaurant ] = useState({heatlevels: {}})
 
     const restaurantid = parseInt(props.history.location.pathname.split("/")[3])
-    
+
     useEffect(() => {
-      getRestaurantById(restaurantid)
+        getRestaurantById(restaurantid)
 
 
     }, [restaurantid])
-    
+
     return (
         <section className="restaurant">
-            {/* <h3 className="restaurant__name">{restaurant.name}</h3> */}
-            <img src={`${restaurant.img}`} className="restaurant__img"></img>
+            <div>
+                <img src={`${restaurant.img}`} className="restaurant__img"></img>
+            </div>
             <a href={restaurant.website} className="restaurant__website">{restaurant.website}</a>
         </section>
     )
