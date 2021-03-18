7785f4ba330eabcca665ddd9681cdaf9af224ca8
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-17 21:52:42 -0500

    created profile view and moved modules in to have a landing page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,25 +1,34 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { RestaurantProvider } from "./restaurant/RestaurantProvider.js"
-import {RestaurantList} from './restaurant/RestaurantList'
+import { RestaurantList } from './restaurant/RestaurantList'
 import { RestaurantDetail } from './restaurant/RestaurantDetail'
+import { RestaurantHeatProvider } from './restaurantheat/RestaurantHeatProvider'
+import { RestaurantHeatList } from "./restaurantheat/RestaurantHeatList.js"
+import { OrderProvider } from "./order/OrderProvider.js"
+import { OrderList } from './order/OrderList'
+import { Profile } from './profile/Profile'
 
 export const ApplicationViews = (props) => {
     return <>
-        <main style={{
+        {/* <main style={{
             margin: "5rem 2rem",
             backgroundColor: "lightgoldenrodyellow"
-        }}>
+        }}> */}
             <RestaurantProvider>
-                <Route path="/" render={(props) => {
-                   return <RestaurantList {...props}/> 
-                }}>
-                </Route>
-                <Route path="/restaurant/" render={(props) => {
-                   return <RestaurantDetail {...props}/> 
-                }}>
-                </Route>
+                <RestaurantHeatProvider>
+                    <OrderProvider>
+                        <Route path={["/", "/restaurant"]} render={(props) => {
+                            return <Profile {...props} />
+                        }}>
+                        </Route>
+                    </OrderProvider>
+                </RestaurantHeatProvider>
             </RestaurantProvider>
-        </main>
+            {/* <Route path="/" render={(props) => {
+                    return <OrderList {...props} />
+                }}>
+                </Route> */}
+        {/* </main> */}
     </>
 }
