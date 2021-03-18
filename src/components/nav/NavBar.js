988e22a8cdfe60097bacf70c8f2faa10f89e8dc1
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-17 21:54:14 -0500

    added Home

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,7 +6,7 @@ export const NavBar = (props) => {
     return (
         <ul className="navbar">
             <li className="navbar__item">
-                Navigation link
+            <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="navbar__item">
                 Navigation link
