d3e5535820d1573f487c64cfc16c48d6036e975e
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-20 23:25:28 -0500

    moved modules to account for common path in app views

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
 import { OrderList } from '../order/OrderList'
+import { OrderForm } from '../order/OrderForm'
 import { RestaurantList } from '../restaurant/RestaurantList'
 import { RestaurantDetail } from '../restaurant/RestaurantDetail'
 import { RestaurantHeatList } from '../restaurantheat/RestaurantHeatList'
@@ -7,15 +8,19 @@ import "./Profile.css"
 
 
 export const Profile = (props) => {
+
+
+
     return (
         <>
+        
             <div className="profile">
                 <div className="profile__card">
                     < OrderList {...props} />
                 </div>
                 <div className="profile__card">
                     < RestaurantList  {...props} />
-                    <RestaurantDetail {...props} />
+                    <OrderForm {...props}/>
                 </div>
                 <div className="profile__card">
                     <RestaurantHeatList {...props} />
