2c28345729b9c7f569e1c782b62c6612e3c8abd7
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-21 20:44:09 -0500

    fixed the boolean errors and tried different things to fix asyn issue but itwas in orderprovider

--- src/components/order/OrderForm.js
+++ src/components/order/OrderForm.js
@@ -1,28 +1,29 @@
 import React, { useContext, useState, useEffect } from 'react'
-import {useParams, useHistory} from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { RestaurantHeatContext } from '../restaurantheat/RestaurantHeatProvider'
 import { OrderContext } from './OrderProvider'
 
 
+
 export const OrderForm = (props) => {
 
-    const { restaurantHeatByRestaurant, getRestaurantHeatByRestaurantId } = useContext(RestaurantHeatContext)
-    const { getOrder, createOrder, editOrder } = useContext(OrderContext)
+    const { rateRestaurantHeat,restaurantHeatByRestaurant, getRestaurantHeatByRestaurantId } = useContext(RestaurantHeatContext)
+    const { getOrder, createOrder, editOrder, getOrdersByUser } = useContext(OrderContext)
     const params = useParams()
     const history = useHistory()
+    let isEnjoyable = ""
     const restaurantid = parseInt(params.restaurantId)
-    const [currentRestaurantHeat, setCurrentRestaurantHeat] = useState(0)
-    const [enjoyableState, setEnjoyableState] = useState("true")
-    const [currentRating, setCurrentRating] = useState({rating: 0})
+    const [currentRating, setCurrentRating] = useState(1)
     const [currentOrder, setCurrentOrder] = useState({
 
         restaurantid: restaurantid,
-        restaurantheatid: parseInt(currentRestaurantHeat),
+        restaurantheatid: 0,
         note: "",
-        enjoyable: enjoyableState
+        enjoyable: false
     })
+    const todayDate = new Date().toISOString().slice(0,10);
 
-     useEffect(() => {
+    useEffect(() => {
         if (isNaN(restaurantid)) {
         } else { getRestaurantHeatByRestaurantId(restaurantid) }
     }, [])
@@ -31,7 +32,7 @@ export const OrderForm = (props) => {
 
         if ("orderId" in props.match.params) {
             getOrder(params.orderId).then(order => {
-                
+
                 setCurrentOrder({
                     restaurantid: order.restaurantid,
                     restaurantheatid: order.restaurantHeatId,
@@ -43,42 +44,57 @@ export const OrderForm = (props) => {
         }
     }, [params.orderId])
 
+    if (currentOrder.enjoyable === "true") {
+        isEnjoyable = true
+    } else if (currentOrder.enjoyable ==="false") {
+        isEnjoyable = false
+    }
+    
+
     const handleControlledInputChange = (event) => {
         const newOrderState = Object.assign({}, currentOrder)
         newOrderState[event.target.name] = event.target.value
         setCurrentOrder(newOrderState)
-
-        const newRestaurantHeatState = event.target.value
-        setCurrentRestaurantHeat(parseInt(newRestaurantHeatState))
-
-        const newEnjoyableState = event.target.value
-        setEnjoyableState(newEnjoyableState)
     }
 
     const handleControlledInputRating = (event) => {
         const newRatingState = event.target.value
         setCurrentRating(newRatingState)
     }
-
+    
+    
     return (
         <form className="orderForm">
             <h5 className="orderForm__title">What'd ya get?</h5>
             <fieldset>
-                <label htmlFor="restaurantheatid"></label>
-                <select type="dropdown" name="restaurantheatid" value={currentRestaurantHeat.id}
-                    onChange={handleControlledInputChange} >
-                    <option value="0">Heat level</option>
-                    {
-                        restaurantHeatByRestaurant.map(rh => (
-                            <option key={rh.id} value={rh.id}>{rh.name}</option>))
-                    }
-                </select>
+                {
+                    restaurantHeatByRestaurant.map(rh => {
+                        
+                        return (
+                            // <ul style={{list-style-type:none}}>
+
+                            <li key={rh.id} >
+                                <input
+                                    type="radio"
+                                    name="restaurantheatid"
+                                    checked={currentOrder.restaurantheatid == rh.id ? true : false}
+                                    value={rh.id}
+                                    
+                                    key={rh.id}
+                                    onChange={handleControlledInputChange} />
+
+                                <label htmlFor="restaurantheatid">{rh.name}</label>
+                            </li>
+                            // </ul>
+                        )
+                    })
+                }
             </fieldset>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="rating">Rate Heat 1-10</label>
+                    <label htmlFor="rating">Rate Heat {`${currentRating}`}</label>
                     <input type="range" name="rating" min="1" max="10" required autoFocus className="form-control"
-                        defaultValue={currentRating.rating}
+                        defaultValue={currentRating}
                         onChange={handleControlledInputRating}
                     />
                 </div>
@@ -95,14 +111,16 @@ export const OrderForm = (props) => {
             <fieldset>
                 <div>
                     <label htmlFor="enjoyable">Yes </label>
-                    <input type="radio" name="enjoyable" 
-                        checked={enjoyableState === "true"}
+                    <input
+                        type="radio"
+                        name="enjoyable"
+                        checked={currentOrder.enjoyable === "true"}
                         value="true"
                         onChange={handleControlledInputChange}
                     />
                     <label htmlFor="enjoyable">No </label>
-                    <input type="radio" name="enjoyable"  
-                        checked={enjoyableState === "false"} value="false"
+                    <input type="radio" name="enjoyable"
+                        checked={currentOrder.enjoyable === "false"} value="false"
                         onChange={handleControlledInputChange}
                     />
                 </div>
@@ -111,7 +129,7 @@ export const OrderForm = (props) => {
                 ("orderId" in props.match.params)
                     ? <button
                         onClick={evt => {
-                            // Prevent form from being submitted
+                            
                             evt.preventDefault()
 
                             editOrder({
@@ -121,32 +139,37 @@ export const OrderForm = (props) => {
                                 note: currentOrder.note,
                                 enjoyable: currentOrder.enjoyable
                             })
-                            
+
                                 .then(() => props.history.push("/"))
                         }}
                         className="btn btn-primary">Edit</button>
                     : <button type="submit"
                         onClick={evt => {
-                            // Prevent form from being submitted
+                            
                             evt.preventDefault()
-
+                            
                             const order = {
 
                                 restaurantId: restaurantid,
-                                restaurantHeatId: currentOrder.parseInt(currentRestaurantHeat),
+                                restaurantHeatId: parseInt(currentOrder.restaurantheatid),
                                 note: currentOrder.note,
-                                enjoyable: currentOrder.enjoyable
+                                enjoyable: isEnjoyable,
+                                createdDate: todayDate
                             }
-                            console.log(order)
-                            // Send POST request to your API
+
+                            const rating = {
+                                rating: currentRating
+                            }                            
                             createOrder(order)
-                                // History is an array that keeps track of all the urls and the browser is on the last
-                                // element in that array so .push is adding ("/") to the end of the array
-                                .then(() => props.history.push("/"))
+                            .then(rateRestaurantHeat(order.restaurantHeatId, rating))
+                            .then(() => history.push("/"))
                         }}
                         className="btn btn-primary">Create</button>
             }
 
-        </form>
+        </form >
     )
+
+    // History is an array that keeps track of all the urls and the browser is on the last
+    // element in that array so .push is adding ("/") to the end of the array
 }
