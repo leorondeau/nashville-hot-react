0d31c52b7414ca3c49e126182c6234fd0c33a9fc
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-23 16:49:28 -0500

    added delete button and ternary

--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import { useParams, useHistory } from 'react-router-dom'
+import React, {useContext} from 'react'
+import { useParams, useHistory,  } from 'react-router-dom'
+import {OrderContext} from './OrderProvider'
 import { Link } from 'react-router-dom'
 import "./Order.css"
 
 export const Order = ({ order }) => {
-
+    const {deleteOrder} = useContext(OrderContext) 
     const params = useParams()
     const history = useHistory()
-    
+    console.log(history)
+    console.log(params)
     return (
         <>
             <section className="order">
@@ -31,6 +33,19 @@ export const Order = ({ order }) => {
                             ? <button className="btn btn-3"
                             onClick={e => history.push(`/orders/${order.id}/edit`)}
                             >Edit</button>
+                            : <div></div>
+                        }
+                        </div>
+                        <div>
+                        {
+                            ("restaurantId" in params)
+                            ? <button className="btn btn-3"
+                            onClick={e => {
+                                if(window.confirm("Delete this order?")){
+                                deleteOrder(order.id)
+                                .then(() => history.push(`/restaurant/${params.restaurantId}`))
+                                }
+                            }}>Delete</button>
                             : <div></div>
                         }
                     </div>
