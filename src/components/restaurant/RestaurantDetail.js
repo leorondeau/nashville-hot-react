2ffa2d73c363bd2074b0756ee708df7073e01009
Leo Rondeau <christopherleorondeau@gmail.com>
2021-03-25 21:53:12 -0500

    styling

--- src/components/restaurant/RestaurantDetail.js
+++ src/components/restaurant/RestaurantDetail.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from 'react'
-import {useParams, useHistory} from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { RestaurantContext } from './RestaurantProvider'
 import "./Restaurant.css"
 import "../profile/Profile.css"
+import flaming from "./flaming.gif"
 
 export const RestaurantDetail = (props) => {
     const { restaurant, getRestaurantById } = useContext(RestaurantContext)
@@ -11,10 +12,10 @@ export const RestaurantDetail = (props) => {
     const restaurantid = parseInt(params.restaurantId)
 
     useEffect(() => {
-        if( restaurantid === 0) {
+        if (restaurantid === 0) {
             history.push("/")
-        } 
-        else {getRestaurantById(restaurantid)}
+        }
+        else { getRestaurantById(restaurantid) }
     }, [restaurantid])
 
     return (
@@ -23,10 +24,18 @@ export const RestaurantDetail = (props) => {
                 <div className="restaurant__website">
                     <a href={restaurant.website} >{restaurant.website}</a>
                 </div>
-                <div >
-                    <img src={`${restaurant.img}`} className="restaurant__img"></img>
+                <div className="image">
+
+                    <div >
+                        <img src={`${restaurant.img}`} className="restaurant__img"></img>
+                    </div>
                 </div>
-                <h5 className="restaurant__suggested_heat">{restaurant.suggested_heat}</h5>
+                <h5>Heat Suggestion:</h5>
+                <div className="suggested-heat-wrapper">
+                    <img src={flaming} className="badass-flame"></img>
+                    <h3 className="restaurant__suggested_heat"> {restaurant.suggested_heat}</h3>
+                    <img src={flaming} className="badass-flame"></img>
+                </div>
             </div>
         </section>
     )
