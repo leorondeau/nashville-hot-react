b4c931168404eeeeecb59e85969af3c75bddff2a
Leo Rondeau <christopherleorondeau@gmail.com>
2021-04-05 22:18:12 -0500

    trying various approaches to get the the button to toggle to show the order list and hide, not currently working

--- src/components/order/OrderList.js
+++ src/components/order/OrderList.js
@@ -9,11 +9,11 @@ import { ProfileContext } from '../profile/ProfileProvider'
 export const OrderList = (props) => {
     const { orders, setOrders, getOrdersByUser, getLimitedOrdersByUser, getOrdersByUserByRestaurantId } = useContext(OrderContext)
     const { profile, getProfile } = useContext(ProfileContext)
-    const [buttonClicked, setButtonClicked] = useState(true)
+ 
+    const [value, setValue] = useState(false)
     const params = useParams()
     const history = useHistory()
     const restaurantid = parseInt(params.restaurantId)
-
 
     useEffect(() => {
         getProfile()
@@ -23,17 +23,20 @@ export const OrderList = (props) => {
         if (isNaN(restaurantid) || restaurantid == 0) {
             getLimitedOrdersByUser()
             // getOrdersByUser()
-     
+
         }
         else {
             (getOrdersByUserByRestaurantId(restaurantid))
         }
 
-        console.log("restId")
+        
     }, [restaurantid])
 
+ 
+
+
     // useEffect(() => {
-        
+
     //     if(buttonClicked == true) {
     //         getLimitedOrdersByUser()
     //     } else {
@@ -41,8 +44,8 @@ export const OrderList = (props) => {
     //     }
     //     console.log("button")
     //     },[buttonClicked])
-  
-   
+
+
 
     return (
         <>
@@ -52,17 +55,23 @@ export const OrderList = (props) => {
                 <header className="restaurants__header restaurant">
                     <h4>Nashville Hot Visits</h4>
                 </header>
-                <div className="restaurant__customer">
-                    <h5>{profile.customer.user.first_name}</h5>
-                    <h5>{profile.customer.heat_tolerance}</h5>
-                </div>
+                <button className="mobile__button" onClick={setValue}>
+                Nashville Hot Visits
+                </button>
+                <div className={value ? "orderlist-hide" : "orderlist"}>
 
-                {
-                    orders.map(o => (
-                        <Order key={o.id} value={o.id} order={o} />
-                    ))
-                }
-                
+                    <div className="restaurant__customer">
+                        <h5>{profile.customer.user.first_name}</h5>
+                        <h5>{profile.customer.heat_tolerance}</h5>
+                    </div>
+
+                    {
+                        orders.map(o => (
+                            <Order key={o.id} value={o.id} order={o} />
+                        ))
+                    }
+
+                </div>
                 {/* <button onClick={e => {
                     buttonClicked ? setButtonClicked(false) :setButtonClicked(true)}
                 }> 
